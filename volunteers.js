fee78cd99e03e13040a8ba92868673a02a4cf459
James Li <60281808+LiYueqian-James@users.noreply.github.com>
2022-09-08 13:26:49 -0500

    Created Volunteers.js

---
+++ volunteers.js
@@ -0,0 +1,18 @@
+/*
+Functions to generate a calendar view for the volunteers. 
+*/
+
+/*
+Get all the new students arriving time.
+Returns: 
+*/
+function getAllStudentsTime()
+
+
+/*
+Functions to be called after volunteers have submitted their pickup requests.
+*/
+
+
+
+
