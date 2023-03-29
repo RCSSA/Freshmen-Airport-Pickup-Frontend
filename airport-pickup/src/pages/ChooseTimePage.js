4bf6fe14a00d10c77c9b5d60e151f1d4659f8359
AlexiaHyn <sunfloweralexia@hotmail.com>
2023-03-28 21:57:26 -0500

    update choosetimepage

--- airport-pickup/src/pages/ChooseTimePage.js
+++ airport-pickup/src/pages/ChooseTimePage.js
@@ -20,40 +20,25 @@ export default function ChooseTimePage() {
     </div>)
   }, [selected])
   return (
-    <div className='row justify-content-center p-5'>
+    <div className='row justify-content-center py-5'>
       <div className='col-12 col-md-8'>
         <h1 className='fw-bold my-4'>志愿者接机时间选择</h1>
-        <div className='my-2'>
+        <div className='mt-0 mb-4'>
           请选择接机时间并完善细节，谢谢~
         </div>
-        {/* <div>
-          <iframe src="https://calendar.google.com/calendar/embed?src=61hqh4guqb0gttl6j5ijubegn4%40group.calendar.google.com&ctz=America%2FChicago" style={{border:"0"}} width={"100%"} height="600" frameBorder="0" scrolling="no"></iframe>
-        </div> */}
-        <div>
-          <FullCalendarModel selected = {selected} setSelected = {setSelected} ids = {ids} titles = {titles} dates = {dates} descriptions = {descriptions}/>
-        </div>
-        <div className='d-flex justify-content-end mt-4'>
-          <button type='button' className='btn btn-info homepage-btn' data-bs-toggle="modal" data-bs-target="#exampleModal">查看并确认选择</button>
-        </div> 
-      </div>
-
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">已选择时间段</h1>
-              
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              {choices}
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">返回</button>
-              <button type="button" className="btn btn-info" onClick={()=>navigate("/choosetime")}>确认并继续</button>
-            </div>
+        <div className='row'>
+          <div className='col-lg-9 col-12'>
+            <FullCalendarModel selected = {selected} setSelected = {setSelected} ids = {ids} titles = {titles} dates = {dates} descriptions = {descriptions}/>
+          </div>
+          <div className='col-lg-3 col-12 pt-3 selectedEventsFrame'>
+            <p>已选时间段：</p>
+            {choices}
           </div>
         </div>
+        
+        <div className='d-flex justify-content-center mt-4'>
+          <button type='button' className='btn btn-info homepage-btn' onClick={()=>navigate("/choosetime")}>确认并提交</button>
+        </div> 
       </div>
     </div>
   )
