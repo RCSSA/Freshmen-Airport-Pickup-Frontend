ffec310abba380b7de7640faba39c2a70c9a516c
AlexiaHyn <sunfloweralexia@hotmail.com>
2023-05-25 23:22:58 +0800

    alexia

--- airport-pickup/src/component/FullCalendar.js
+++ airport-pickup/src/component/FullCalendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"
@@ -10,6 +10,7 @@ export default function FullCalendarModel(props) {
   const [loaded, setLoaded] = useState(false);
   var [events, setEvents] = useState([]);
   var [openModals, setOpenModals] = useState({});
+  const calendarRef = useRef(null);
 
   function loadEvents(){
     events = []
@@ -19,9 +20,7 @@ export default function FullCalendarModel(props) {
       events.push({
           id: `${id}`,
           title: `${props.titles[i]}`, 
-          date: `${props.dates[i]}`,
-          classNames: ['selectedEvent'],
-          description: `${props.descriptions[i]}`,    
+          date: `${props.dates[i]}`,   
           backgroundColor: eventColors[[id]],
       })
       setEvents([...events])
@@ -29,15 +28,18 @@ export default function FullCalendarModel(props) {
   }
   
   useEffect(()=>{
-    for (var i = 0; i < props.ids.length; i++) {
-      var id = props.ids[i]
-      eventColors[`${props.ids[i]}`]= "rgb(14, 119, 217)";
-      setEventColors({...eventColors, [`${props.ids[i]}`]: "rgb(14, 119, 217)"});
-      openModals[`${props.ids[i]}`] = false;
-      setOpenModals({...openModals, [`${props.ids[i]}`]: false});
+    
+      for (var i = 0; i < props.ids.length; i++) {
+        var id = props.ids[i]
+        eventColors[`${id}`]= "rgb(14, 119, 217)";
+        setEventColors({...eventColors, [`${id}`]: "rgb(14, 119, 217)"});
+        openModals[`${id}`] = false;
+        setOpenModals({...openModals, [`${id}`]: false});
+      }
       loadEvents();
-    setLoaded(true);
-  }}, [])
+      setLoaded(true);
+    
+  }, [])
 
   useEffect(()=>{
     loadEvents()
@@ -47,16 +49,6 @@ export default function FullCalendarModel(props) {
     const eventId = info.event.id;
     openModals[eventId] = true;
     setOpenModals({...openModals, [`${eventId}`]: true});
-
-      // if (props.selected.includes(eventId)){
-      //   props.setSelected(props.selected.filter((elem)=>{return elem != eventId}));
-      //   eventColors[[eventId]] = "rgb(14, 119, 217)"
-      //   setEventColors({...eventColors, [`${eventId}`]: "rgb(14, 119, 217)"});
-      // } else {
-      //   props.setSelected([...props.selected, eventId]);
-      //   eventColors[[eventId]] = "rgb(73, 246, 255)"
-      //   setEventColors({...eventColors, [`${eventId}`]: "rgb(73, 246, 255)"});
-      // }
   }
     
   return (
@@ -70,6 +62,7 @@ export default function FullCalendarModel(props) {
             return (<EventInfoModal
             id = {id}
             key = {idx}
+            date = {props.dates[idx]}
             openModals = {openModals}
             setOpenModals = {setOpenModals}
             description = {props.descriptions[idx]}
@@ -77,15 +70,20 @@ export default function FullCalendarModel(props) {
             selected = {props.selected}
             eventColors = {eventColors}
             setEventColors = {setEventColors}
+            pickUpNumber = {props.pickUpNumber}
+            setPickUpNumber = {props.setPickUpNumber}
             />)
           })
         }
       </div>
       
       <FullCalendar
+        ref={calendarRef}
           plugins={[ dayGridPlugin, interactionPlugin ]}
           initialView="dayGridMonth"
           events={events}
+          eventMouseEnter={()=>{document.body.style.cursor = 'pointer'}}
+          eventMouseLeave={()=>{document.body.style.cursor = 'auto'}}
           eventClick={(info)=>handleEventClick(info)}
           eventTimeFormat= {[ // like '14:30:00'
             {hour: '2-digit'},
