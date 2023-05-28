05ec602e383a77064563e1a82929d15ae8afdd73
Huang-Ge <gehuang38@gmail.com>
2023-05-28 13:10:37 +0800

    change root

--- new_students.js
+++
@@ -1,6 +0,0 @@
-/**
-* After a new student submitted their information, save these data to the google sheet. 
-* @param  {String} name
-*
-**/
-function saveNewStudentInfo(name, email, wechat, dateTime, airport)
