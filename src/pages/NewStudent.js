eb3879ae2f73fb2ea33c583e37cd344148b40f89
Huang-Ge <gehuang38@gmail.com>
2023-06-03 22:43:17 -0500

    add data store

--- src/pages/NewStudent.js
+++ src/pages/NewStudent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 import DateTimePicker from "react-datetime-picker";
 import "react-datetime-picker/dist/DateTimePicker.css";
@@ -7,7 +7,32 @@ import "react-clock/dist/Clock.css";
 
 export default function NewStudentPage() {
   //   const navigate = useNavigate();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [riceID, setRiceID] = useState("");
+  const [flightNumber, setFlightNumber] = useState("");
+  const [airport, setAirport] = useState("");
   const [arriveDate, setArriveDate] = useState(new Date());
+
+  useEffect(() => {
+    console.log(`firstname: ${firstName} \n`);
+    console.log(`lastname: ${lastName} \n`);
+    console.log(`phonenumber: ${phoneNumber} \n`);
+    console.log(`riceid: ${riceID} \n`);
+    console.log(`flightnumber: ${flightNumber} \n`);
+    console.log(`airport: ${airport} \n`);
+    console.log(`arrivedate: ${arriveDate} \n`);
+  }, [
+    firstName,
+    lastName,
+    phoneNumber,
+    riceID,
+    flightNumber,
+    airport,
+    arriveDate,
+  ]);
+
   return (
     <div class="justify-content-center form">
       <div class="row spacing">
@@ -23,6 +48,7 @@ export default function NewStudentPage() {
                   class="form-control"
                   id="validationCustom01"
                   placeholder="First name"
+                  onChange={(e) => setFirstName(e.target.value)}
                   required
                 />
                 <div class="valid-feedback">Looks good!</div>
@@ -34,6 +60,7 @@ export default function NewStudentPage() {
                   class="form-control"
                   id="validationCustom02"
                   placeholder="Last name"
+                  onChange={(e) => setLastName(e.target.value)}
                   required
                 />
                 <div class="valid-feedback">Looks good!</div>
@@ -47,6 +74,7 @@ export default function NewStudentPage() {
                   class="form-control"
                   id="validationCustom03"
                   placeholder="Phone Number"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
                   required
                 />
                 <div class="invalid-feedback">
@@ -57,15 +85,34 @@ export default function NewStudentPage() {
 
             <div class="row form-row">
               <div class="col-md-6 mb-3">
-                <label for="validationCustom03">Rice ID</label>
+                <label for="validationCustom05">Rice ID</label>
                 <input
                   type="text"
                   class="form-control"
                   id="validationCustom05"
-                  placeholder="Rice ID (example: yk01)"
+                  placeholder="Rice ID (example: yw01)"
+                  onChange={(e) => setRiceID(e.target.value)}
                   required
                 />
+
                 <div class="invalid-feedback">Please provide a rice id.</div>
+              </div>
+            </div>
+
+            <div class="row form-row">
+              <div class="col-md-6 mb-3">
+                <label for="validationCustom06">Flight Number</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="validationCustom06"
+                  placeholder="Flight Number (example: NH114)"
+                  onChange={(e) => setFlightNumber(e.target.value)}
+                  required
+                />
+                <div class="invalid-feedback">
+                  Please provide a Flight Number.
+                </div>
               </div>
             </div>
 
@@ -79,7 +126,8 @@ export default function NewStudentPage() {
                     name="gridRadios"
                     id="gridRadios1"
                     value="option1"
-                    checked
+                    onClick={() => setAirport("IAH")}
+                    defaultChecked
                   />
                   <label class="form-check-label" for="gridRadios1">
                     Houston Intercontinental Airport(IAH)
@@ -92,6 +140,7 @@ export default function NewStudentPage() {
                     name="gridRadios"
                     id="gridRadios2"
                     value="option2"
+                    onClick={() => setAirport("HOU")}
                   />
                   <label class="form-check-label" for="gridRadios2">
                     William P. Hobby Airport(HOU)
