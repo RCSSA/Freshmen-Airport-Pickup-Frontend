d14ed17a50e8dec77d6012b5b5d7b9cec09a5fe3
Huang-Ge <gehuang38@gmail.com>
2023-06-10 13:51:01 -0500

    add new student page

--- airport-pickup/src/App.js
+++ airport-pickup/src/App.js
@@ -1,10 +1,11 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import Homepage from "./pages/Homepage";
 import Registerpage from "./pages/Registerpage";
 import Loginpage from "./pages/Loginpage";
 import ChooseTimePage from "./pages/ChooseTimePage";
 import TimeInfoPage from "./pages/TimeInfoPage";
+import NewStudentPage from "./pages/newStudentPage";
 import FullCalendarTest from "./component/FullCalendar";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -14,16 +15,16 @@ import "./stylings/styleAH.css";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage/>}></Route>
-      <Route path="/login" element={<Loginpage/>}></Route>
-      <Route path="/register" element={<Registerpage/>}></Route>
-      <Route path="/choosetime" element={<ChooseTimePage/>}></Route>
-      <Route path="/info" element={<TimeInfoPage/>}></Route>
-      <Route path="/calendartest" element={<FullCalendarTest/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/login" element={<Loginpage />}></Route>
+        <Route path="/register" element={<Registerpage />}></Route>
+        <Route path="/choosetime" element={<ChooseTimePage />}></Route>
+        <Route path="/info" element={<TimeInfoPage />}></Route>
+        <Route path="/calendartest" element={<FullCalendarTest />}></Route>
+        <Route path="/nstudent" element={<NewStudentPage />}></Route>
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
