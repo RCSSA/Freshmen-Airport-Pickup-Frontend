dfbe370d7d83f508421b9bf26c9d2f9cb19d7bf8
Huang-Ge <gehuang38@gmail.com>
2023-06-17 23:10:41 -0500

    refactor and fix choose time bug

--- airport-pickup/src/component/EventInfoModal.js
+++ airport-pickup/src/component/EventInfoModal.js
@@ -1,117 +1,156 @@
-import React, {useRef, useState} from 'react'
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
 
 export default function EventInfoModal(props) {
-    const eventId = props.id;
-    const openModals = props.openModals;
-    const setOpenModals = props.setOpenModals;
-    const eventColors = props.eventColors;
-    const setEventColors = props.setEventColors;
-    const [select, setSelect] = useState("确认选择");
+  const eventId = props.id;
+  const openModals = props.openModals;
+  const setOpenModals = props.setOpenModals;
+  const eventColors = props.eventColors;
+  const setEventColors = props.setEventColors;
+  const [select, setSelect] = useState("确认选择");
 
-    function checkAllZero(){
-        const checkNumbers = props.pickUpNumber[`${props.id}`];
-        var sum = checkNumbers.reduce((acc, currentValue) => {
-            return acc + parseInt(currentValue)
-          }, 0);
+  function checkAllZero() {
+    const checkNumbers = props.pickUpNumber[`${props.id}`];
+    var sum = checkNumbers.reduce((acc, currentValue) => {
+      return acc + parseInt(currentValue);
+    }, 0);
 
-        if (sum === 0) {
-            return true;
+    if (sum === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  function updateSelection() {
+    const checkNumbers = props.pickUpNumber[`${props.id}`];
+    const selections = [eventId];
+    for (var i = 0; i < checkNumbers.length; i++) {
+      if (checkNumbers[i] > 0) {
+        let selection =
+          props.date +
+          " " +
+          props.title.substring(0, 3) +
+          " " +
+          props.description[i][0] +
+          ": " +
+          checkNumbers[i] +
+          "人";
+        selections.push(selection);
+      }
+    }
+    return selections;
+  }
+
+  function handleClose() {
+    props.setSelected(
+      props.selected.filter((elem) => {
+        return elem[0] !== eventId;
+      })
+    );
+    if (checkAllZero()) {
+      setSelect("确认选择");
+      eventColors[[eventId]] = "rgb(14, 119, 217)";
+      setEventColors({ ...eventColors, [`${eventId}`]: "rgb(14, 119, 217)" });
+    } else {
+      setSelect("更新选择");
+      const selections = updateSelection();
+      var selectedCopy = props.selected;
+      var i = 0;
+      var repeat = false;
+      while (i < props.selected.length) {
+        if (props.selected[i][0] === eventId) {
+          repeat = true;
+          break;
         }
-        return false;
+        i += 1;
+      }
+      if (repeat) {
+        selectedCopy[i] = selections;
+        props.setSelected([...selectedCopy]);
+      } else {
+        props.setSelected([...props.selected, selections]);
+      }
+
+      eventColors[[eventId]] = "#0dcafd";
+      setEventColors({ ...eventColors, [`${eventId}`]: "#0dcafd" });
     }
 
-    function updateSelection(){
-        const checkNumbers = props.pickUpNumber[`${props.id}`];
-        const selections = [eventId]
-        for (var i = 0; i < checkNumbers.length; i++){
-            if (checkNumbers[i] > 0) {
-                let selection = props.date + " " + props.title.substring(0, 3) + " " + props.description[i][0] + ": " + checkNumbers[i] + "人"
-                selections.push(selection)
-            }
-        }
-        return selections
-    }
+    openModals[eventId] = false;
+    setOpenModals({ ...openModals, [`${eventId}`]: false });
+  }
 
-    function handleClose(){
-        openModals[eventId] = false;
-        setOpenModals({...openModals, [`${eventId}`]: false});
-    }
+  function handleSelect(e) {
+    e.preventDefault();
 
-    function handleSelect(e){
-        e.preventDefault();
-        props.setSelected(props.selected.filter((elem)=>{
-            return elem[0] !== eventId}));
-        if (checkAllZero()){
-            setSelect("确认选择");
-            eventColors[[eventId]] = "rgb(14, 119, 217)"
-            setEventColors({...eventColors, [`${eventId}`]: "rgb(14, 119, 217)"});
-        } else {
-            setSelect("更新选择");
-            const selections = updateSelection();
-            var selectedCopy = props.selected;
-            var i = 0;
-            var repeat = false;
-            while (i < props.selected.length){
-                if (props.selected[i][0] === eventId) {
-                    repeat = true;
-                    break;
-                }
-                i += 1;
-            }
-            if (repeat){
-                selectedCopy[i] = selections
-                props.setSelected([...selectedCopy])
-
-            } else {
-                props.setSelected([...props.selected, selections])
-            }
-
-            eventColors[[eventId]] = "#0dcafd"
-            setEventColors({...eventColors, [`${eventId}`]: "#0dcafd"});
-        }
-    }
+    handleClose();
+  }
 
   return (
     <Modal
-        open = {openModals[eventId]}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        className='eventInfoModalFrame d-flex justify-content-center align-items-center'
+      open={openModals[eventId]}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      className="eventInfoModalFrame d-flex justify-content-center align-items-center"
     >
-        <div className='eventInfoModal bg-light p-5'>
-            <Typography id="modal-modal-title" variant="h6" component="h2" className='text-center fw-bold'>
-                时间段信息
-            </Typography>
-            <div className='mt-2'>请输入每个时间段计划的接机人数，并点击“选择”。</div>
-            <div className='color-light-blue'>友情提醒：新生普遍会携带两个大号托运箱和一个登机箱，请您也考虑行李所需的空间。<br/>（家用轿车，小型suv大概可以接下两位新生，大suv可以接下三位新生）</div>
-            <form className='d-flex flex-column mt-2' onSubmit={handleSelect}>
-                {props.description.map((time, idx)=>{
-                    return (
-                    <div key={idx} className='d-flex justify-content-between align-items-center mb-2'>
-                        <div className='d-flex'>{`${time[0] + ":"}`}<div className='mx-2'>{`${time[1] + "人"}`}</div></div>
-                        <div className='d-flex align-items-center'>
-                            <div>接机人数：</div>
-                            <input type='number'min={0} max={time[1]} className='form-control ms-2 text-center' style={{width: "100px"}} 
-                            name={time[0]}
-                            value={props.pickUpNumber[`${props.id}`][idx]}
-                            onChange={(e)=>{
-                                let pickUpNumberPart = props.pickUpNumber[`${props.id}`]
-                                pickUpNumberPart[idx] = e.target.value;
-                                props.pickUpNumber[`${props.id}`][idx] = e.target.value; 
-                                props.setPickUpNumber({...props.pickUpNumber, [`${props.id}`]: pickUpNumberPart})}}
-                            />
-                        </div>
-                    </div>
-                    )
-                })}
-                <button className='btn btn-primary' type='submit'>
-                    {select}
-                </button>
-            </form>
+      <div className="eventInfoModal bg-light p-5">
+        <Typography
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+          className="text-center fw-bold"
+        >
+          时间段信息
+        </Typography>
+        <div className="mt-2">
+          请输入每个时间段计划的接机人数，并点击“选择”。
         </div>
-  </Modal>
-  )
+        <div className="color-light-blue">
+          友情提醒：新生普遍会携带两个大号托运箱和一个登机箱，请您也考虑行李所需的空间。
+          <br />
+          （家用轿车，小型suv大概可以接下两位新生，大suv可以接下三位新生）
+        </div>
+        <form className="d-flex flex-column mt-2" onSubmit={handleSelect}>
+          {props.description.map((time, idx) => {
+            return (
+              <div
+                key={idx}
+                className="d-flex justify-content-between align-items-center mb-2"
+              >
+                <div className="d-flex">
+                  {`${time[0] + ":"}`}
+                  <div className="mx-2">{`${time[1] + "人"}`}</div>
+                </div>
+                <div className="d-flex align-items-center">
+                  <div>接机人数：</div>
+                  <input
+                    type="number"
+                    min={0}
+                    max={time[1]}
+                    className="form-control ms-2 text-center"
+                    style={{ width: "100px" }}
+                    name={time[0]}
+                    value={props.pickUpNumber[`${props.id}`][idx]}
+                    onChange={(e) => {
+                      let pickUpNumberPart = props.pickUpNumber[`${props.id}`];
+                      pickUpNumberPart[idx] = e.target.value;
+                      props.pickUpNumber[`${props.id}`][idx] = e.target.value;
+                      props.setPickUpNumber({
+                        ...props.pickUpNumber,
+                        [`${props.id}`]: pickUpNumberPart,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+          <button className="btn btn-primary" type="submit">
+            {select}
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
 }
--- airport-pickup/src/pages/ChooseTimePage.js
+++ airport-pickup/src/pages/ChooseTimePage.js
@@ -1,158 +1,183 @@
-import React, {useCallback, useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import FullCalendarModel from '../component/FullCalendar';
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FullCalendarModel from "../component/FullCalendar";
+import LoadingPage from "./LoadingPage";
 export default function ChooseTimePage() {
   const navigate = useNavigate();
   const [loaded, setLoaded] = useState(false);
-  const [selected, setSelected] = useState([]);  // note down selected event
+  const [selected, setSelected] = useState([]); // note down selected event
   const [choices, setChoices] = useState(<div></div>); // for showing of choices on the right side
   const [pickUpNumber, setPickUpNumber] = useState({}); // note down number of pickups in each event
 
   var sampleData = {
-    "8": {
-      "date": "2023-05-13",
-      "HOU": {"TotalToBePicked": 3,
-              "10:00-12:00": 2,
-              "14:00-16:00": 1,},
-      "IAH": {"TotalToBePicked": 8,
-            "10:00-12:00": 2,
-            "14:00-16:00": 4,
-            "16:00-18:00": 2},
+    8: {
+      date: "2023-05-13",
+      HOU: { TotalToBePicked: 3, "10:00-12:00": 2, "14:00-16:00": 1 },
+      IAH: {
+        TotalToBePicked: 8,
+        "10:00-12:00": 2,
+        "14:00-16:00": 4,
+        "16:00-18:00": 2,
+      },
     },
 
-    "2": {
-      "date": "2023-05-14",
-      "HOU": {"TotalToBePicked": 10,
-              "10:00-12:00": 2,
-              "14:00-16:00": 5,
-              "16:00-18:00": 3},
-      "IAH": {"TotalToBePicked": 7,
-              "10:00-12:00": 1,
-              "14:00-16:00": 4,
-              "16:00-18:00": 2},
+    2: {
+      date: "2023-05-14",
+      HOU: {
+        TotalToBePicked: 10,
+        "10:00-12:00": 2,
+        "14:00-16:00": 5,
+        "16:00-18:00": 3,
+      },
+      IAH: {
+        TotalToBePicked: 7,
+        "10:00-12:00": 1,
+        "14:00-16:00": 4,
+        "16:00-18:00": 2,
+      },
     },
 
-    "5": {
-      "date": "2023-05-17",
-      "IAH": {"TotalToBePicked": 5,
-              "10:00-12:00": 1,
-              "14:00-16:00": 2,
-              "16:00-18:00": 2},
+    5: {
+      date: "2023-05-17",
+      IAH: {
+        TotalToBePicked: 5,
+        "10:00-12:00": 1,
+        "14:00-16:00": 2,
+        "16:00-18:00": 2,
+      },
     },
-  }
+  };
 
   var [fullCalendarEvents, setFullCalendarEvents] = useState([]);
 
-  const dataManagement = useCallback(() => {
-    let idCount = 0;
-    let idList = [], titles = [], dates = [], descriptions = [], pickUpStructures = [];
-    let ids = Object.keys(sampleData)
-    for (var i = 0; i < ids.length; i++){
-      const key1 = ids[i];
-      const date = sampleData[key1]["date"];
-      const keys2 = Object.keys(sampleData[key1]);
-      if (keys2.includes("IAH")){
-        idCount += 1;
-        titles.push("IAH: " + sampleData[key1]["IAH"]["TotalToBePicked"] + " 人")
-        dates.push(date);
-  
-        const keys3 = Object.keys(sampleData[key1]["IAH"]).filter((key3)=>{return (key3 !== "TotalToBePicked")});
-        let description = [];
-        let pickUpStructure = [];
-        for (var j = 0; j < keys3.length; j++){
-          description.push([keys3[j], sampleData[key1]["IAH"][keys3[j]]])
-          pickUpStructure.push(0);
+  const dataManagement = useCallback(
+    (sampleData) => {
+      let idCount = 0;
+      let idList = [],
+        titles = [],
+        dates = [],
+        descriptions = [],
+        pickUpStructures = [];
+      let ids = Object.keys(sampleData);
+      for (var i = 0; i < ids.length; i++) {
+        const key1 = ids[i];
+        const date = sampleData[key1]["date"];
+        const keys2 = Object.keys(sampleData[key1]);
+        if (keys2.includes("IAH")) {
+          idCount += 1;
+          titles.push(
+            "IAH: " + sampleData[key1]["IAH"]["TotalToBePicked"] + " 人"
+          );
+          dates.push(date);
+
+          const keys3 = Object.keys(sampleData[key1]["IAH"]).filter((key3) => {
+            return key3 !== "TotalToBePicked";
+          });
+          let description = [];
+          let pickUpStructure = [];
+          for (var j = 0; j < keys3.length; j++) {
+            description.push([keys3[j], sampleData[key1]["IAH"][keys3[j]]]);
+            pickUpStructure.push(0);
+          }
+          descriptions.push(description);
+          pickUpStructures.push(pickUpStructure);
         }
-        descriptions.push(description);
-        pickUpStructures.push(pickUpStructure);
-      }
-  
-      if (keys2.includes("HOU")){
-        idCount += 1;
-        titles.push("HOU: " + sampleData[key1]["HOU"]["TotalToBePicked"] + " 人")
-        dates.push(date);
-        
-        const keys3 = Object.keys(sampleData[key1]["HOU"]).filter((key3)=>{return (key3 !== "TotalToBePicked")});
-        let description = [];
-        let pickUpStructure = [];
-        for (var j = 0; j < keys3.length; j++){
-          description.push([keys3[j], sampleData[key1]["HOU"][keys3[j]]])
-          pickUpStructure.push(0);
+
+        if (keys2.includes("HOU")) {
+          idCount += 1;
+          titles.push(
+            "HOU: " + sampleData[key1]["HOU"]["TotalToBePicked"] + " 人"
+          );
+          dates.push(date);
+
+          const keys3 = Object.keys(sampleData[key1]["HOU"]).filter((key3) => {
+            return key3 !== "TotalToBePicked";
+          });
+          let description = [];
+          let pickUpStructure = [];
+          for (var j = 0; j < keys3.length; j++) {
+            description.push([keys3[j], sampleData[key1]["HOU"][keys3[j]]]);
+            pickUpStructure.push(0);
+          }
+          descriptions.push(description);
+          pickUpStructures.push(pickUpStructure);
         }
-        descriptions.push(description);
-        pickUpStructures.push(pickUpStructure);
       }
-    }
-  
-    for (var i = 0; i < idCount; i++){
-      idList.push(i);
-      pickUpNumber[`${i}`] = pickUpStructures[i];
-      setPickUpNumber({...pickUpNumber, [`${i}`]: pickUpStructures[i]})
-    }
 
-    return ([idList, titles, dates, descriptions])
-  }, [sampleData, pickUpNumber])
+      for (var i = 0; i < idCount; i++) {
+        idList.push(i);
+        pickUpNumber[`${i}`] = pickUpStructures[i];
+        setPickUpNumber({ ...pickUpNumber, [`${i}`]: pickUpStructures[i] });
+      }
 
+      return [idList, titles, dates, descriptions];
+    },
+    [sampleData, pickUpNumber]
+  );
 
-  useEffect(()=>{
-    let [a, b, c, d] = dataManagement();
+  useEffect(() => {
+    let [a, b, c, d] = dataManagement(sampleData);
     fullCalendarEvents = [a, b, c, d];
-    setFullCalendarEvents([...fullCalendarEvents, a, b, c, d])
-    setTimeout(()=>{setLoaded(true);}, 200)
-  }, [])
+    setFullCalendarEvents([...fullCalendarEvents, a, b, c, d]);
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
 
-
-  useEffect(()=>{
-    setChoices( 
-    <div>
-      {
-        selected.map((day, key) => {
+  useEffect(() => {
+    setChoices(
+      <div>
+        {selected.map((day, key) => {
           return (
-            <div key={key} style={{fontSize: "12px"}}>
-              {day.map((event, key2)=>{
-                if (key2 !== 0){
-                  return <div key={key2}>{event}</div>
+            <div key={key} style={{ fontSize: "12px" }}>
+              {day.map((event, key2) => {
+                if (key2 !== 0) {
+                  return <div key={key2}>{event}</div>;
                 }
               })}
             </div>
-          )
-        })
-      }
-    </div>)
-  }, [selected])
+          );
+        })}
+      </div>
+    );
+  }, [selected]);
 
-  return (
-    loaded?
-    <div className='row justify-content-center py-5'>
-      <div className='col-12 col-md-8'>
-        <h1 className='fw-bold my-4'>志愿者接机时间选择</h1>
-        <div className='mt-0 mb-4'>
-          请选择接机时间并完善细节，谢谢~
-        </div>
-        <div className='row'>
-          <div className='col-lg-9 col-12'>
-            <FullCalendarModel selected = {selected} setSelected = {setSelected} 
-            ids = {fullCalendarEvents[0]} 
-            titles = {fullCalendarEvents[1]} 
-            dates = {fullCalendarEvents[2]} 
-            descriptions = {fullCalendarEvents[3]}
-            pickUpNumber = {pickUpNumber}
-            setPickUpNumber = {setPickUpNumber}
+  return loaded ? (
+    <div className="row justify-content-center py-5">
+      <div className="col-12 col-md-8">
+        <h1 className="fw-bold my-4">志愿者接机时间选择</h1>
+        <div className="mt-0 mb-4">请选择接机时间并完善细节，谢谢~</div>
+        <div className="row">
+          <div className="col-lg-9 col-12">
+            <FullCalendarModel
+              selected={selected}
+              setSelected={setSelected}
+              ids={fullCalendarEvents[0]}
+              titles={fullCalendarEvents[1]}
+              dates={fullCalendarEvents[2]}
+              descriptions={fullCalendarEvents[3]}
+              pickUpNumber={pickUpNumber}
+              setPickUpNumber={setPickUpNumber}
             />
-            
           </div>
-          <div className='col-lg-3 col-12 pt-3 selectedEventsFrame'>
-            <p className='fw-bold'>已选时间段和人数：</p>
+          <div className="col-lg-3 col-12 pt-3 selectedEventsFrame">
+            <p className="fw-bold">已选时间段和人数：</p>
             {choices}
           </div>
         </div>
-        
-        <div className='d-flex justify-content-center mt-4'>
-          <button type='button' className='btn btn-info homepage-btn' onClick={()=>navigate("/info")}>下一步</button>
-        </div> 
+
+        <div className="d-flex justify-content-center mt-4">
+          <button
+            type="button"
+            className="btn btn-info homepage-btn"
+            onClick={() => navigate("/info")}
+          >
+            下一步
+          </button>
+        </div>
       </div>
     </div>
-    :
-    <div>loading...</div>
-  )
+  ) : (
+    <LoadingPage />
+  );
 }
