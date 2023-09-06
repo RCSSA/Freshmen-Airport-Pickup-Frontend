b2125c04fe413fb0e43a676514ff6cfd77b99c39
Huang-Ge <gehuang38@gmail.com>
2023-09-05 22:59:18 -0500

    remove extra folder

--- new_students.js
+++
@@ -1,8 +0,0 @@
-/**
-* After a new student submitted their information, save these data to the google sheet. 
-* @param  {String} name
-*
-**/
-function saveNewStudentInfo()
-
-function queryStudentStatus()
