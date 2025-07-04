1f272e24831b0a34d07e3cc8f656288597e6abfc
Huaijin Sun <huaijinsun@unknown46ed00a95caf.attlocal.net>
2025-07-03 22:26:00 -0500

    remove max requirement of 10 students on frontend

--- src/pages/volunteers/ChooseTimePage.js
+++ src/pages/volunteers/ChooseTimePage.js
@@ -189,7 +189,7 @@ export default function ChooseTimePage(props) {
       <div className="col-12 col-md-8">
         <h1 className="fw-bold my-4">志愿者接机时间选择</h1>
         <div className="mt-0 mb-4">
-          请选择接机时间并完善细节，每名志愿者最多选择10名新生，谢谢~
+          请选择接机时间并完善细节，谢谢~
         </div>
         <div className="row">
           <div className="col-lg-9 col-12">
